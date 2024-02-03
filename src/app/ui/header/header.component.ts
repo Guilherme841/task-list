48843e3f0c119152861c0ad4d0eef558059f54af
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-03 11:42:50 -0300

    update

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { HeaderService } from '../../services/header-service/header.service';
 
 @Component({
@@ -11,6 +11,16 @@ import { HeaderService } from '../../services/header-service/header.service';
 })
 export class HeaderComponent {
   showMenu: boolean = false;
+  @ViewChild('menuIcon') menuIcon!: ElementRef;
+  @HostListener('document:click', ['$event']) outClickMenu(event: Event) {
+    if (
+      !this.menuIcon.nativeElement.contains(event.target) &&
+      this.showMenu === true
+    ) {
+      this.showMenu = false;
+      HeaderService.toggleMenu.emit();
+    }
+  }
   constructor(private _headerService: HeaderService) {}
   toggleMenu() {
     HeaderService.toggleMenu.emit();
