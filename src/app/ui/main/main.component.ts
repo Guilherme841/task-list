5073658ff4a53dacdeb699bdba38d68208e4c0ca
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-05 13:46:03 -0300

    Revert "update"
    This reverts commit ce45d4525f167bcb2c6c1a516ae7a399925bbc6b.

--- src/app/ui/main/main.component.ts
+++ src/app/ui/main/main.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 import { PopUpAdicionarComponent } from '../../components/pop-up-adicionar/pop-up-adicionar.component';
 import { HeaderService } from '../../services/header-service/header.service';
-import { ContainerTaskComponent } from '../../components/container-task/container-task.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SideBarComponent, PopUpAdicionarComponent, ContainerTaskComponent],
+  imports: [SideBarComponent, PopUpAdicionarComponent],
   providers: [HeaderService],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
 export class MainComponent implements OnInit {
-  ngOnInit(): void {}
+  taskName: string = 'Aqui vai o Nome da Tarefa!';
+  @ViewChild('btnAdd', { static: false }) btnAdd!: ElementRef;
+  constructor(private _headerService: HeaderService) {}
+  callTogglePopUp() {
+    HeaderService.togglePopUp.emit(this.btnAdd);
+  }
+  ngOnInit(): void {
+    
+  }
 }
