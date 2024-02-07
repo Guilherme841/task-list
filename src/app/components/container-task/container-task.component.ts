29b42aef727fd58ae48089e1e4fdf1a661e9e43f
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-07 12:03:32 -0300

    update

--- src/app/components/container-task/container-task.component.ts
+++ src/app/components/container-task/container-task.component.ts
@@ -2,19 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { CommonModule } from '@angular/common';
 
+interface Task {
+  name: string;
+  desc: string;
+}
+
 @Component({
   selector: 'app-container-task',
   standalone: true,
-  imports: [],
-  providers: [TaskService, CommonModule],
+  imports: [CommonModule],
+  providers: [TaskService],
   templateUrl: './container-task.component.html',
   styleUrl: './container-task.component.scss',
 })
 export class ContainerTaskComponent implements OnInit {
   constructor(private _taskService: TaskService) {}
+  nameTask: string = '';
+  descTask: string = '';
+  arrTask: Task[] = [
+    {
+      name: '',
+      desc: '',
+    },
+  ];
   ngOnInit(): void {
     TaskService.addTask.subscribe((task: any) => {
-      console.log(task.name, task.desc);
+      this.arrTask.push(task);
+      this.nameTask = task.name;
+      this.descTask = task.desc;
     });
   }
+  addElement() {}
 }
